b20f4d654da63c3e0a0847e0f78634694728899f
WonMian <478034625@qq.com>
2017-03-29 21:04:09 +0800

    inverted

--- rename.py
+++ rename.py
@@ -8,9 +8,7 @@ path = '/Users/wangmian/PycharmProjects/ShanghaiZhengquan/testlist'
 for filename in os.listdir(path):
     oldname = path + '/' + filename
     filename = path + '/' + filename
-    print oldname + '\n'
     filename = filename.replace(' ','')
     filename = filename.replace('\t','')
-    print filename + '\n'
     os.rename(oldname,filename)
 
