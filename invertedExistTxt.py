ce2c2853d9b39edd080b2a41e105d43d36b90806
WonMian <478034625@qq.com>
2017-04-07 17:51:41 +0800

    inverted

--- invertedExistTxt.py
+++ invertedExistTxt.py
@@ -5,20 +5,19 @@ import os
 import config
 def invertedNow():
     f = open(config.path + '/haveInverted.txt', 'ra+')
-    haveInvertedName = f.readlines()
+    haveInvertedName = f.read()
 
     for filename in os.listdir(config.path + '/txtlistSH'):
         # for filename in filenameList:
         filename = filename.replace('.txt', '')
-        for a in haveInvertedName:
-            if filename in a:
-                print filename + '已经过处理，将不再进行处理'
-            else:
-                try:
-                    invertedAPI(filename)
-                    f.write(filename + '\n')
-                    print '已处理' + filename
-                except Exception, e:
-                    print Exception, ":", e
+        if filename in haveInvertedName:
+            print filename + '已经过处理，将不再进行处理'
+        else:
+            try:
+                invertedAPI(filename)
+                f.write(filename + '\n')
+                print '已处理' + filename
+            except Exception, e:
+                print Exception, ":", e
 
 invertedNow()
